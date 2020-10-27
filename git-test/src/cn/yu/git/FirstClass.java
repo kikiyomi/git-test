69d7dbce1c090ad0520b7f5833960906ca1f87d6
喻小弟 <820941141@qq.com>
2020-10-28 02:26:08 +0800

    把工程添加到本地仓库

---
+++ git-test/src/cn/yu/git/FirstClass.java
@@ -0,0 +1,37 @@
+package cn.yu.git;
+
+/**
+ * @author 陈彦霖
+ * @title: FirstClass
+ * @projectName git-test
+ * @description: TODO
+ * @date 2020/10/28 2:21
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+        System.out.println("hahaha");
+    }
+    public void test(){
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+    }
+}
